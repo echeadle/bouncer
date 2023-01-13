f94de6565430f2dc910733b3b2c1f3eeaf3147e5
echeadle <echeadle@gmail.com>
2023-01-12 19:10:08 -0700

    Finished lesson 17

---
+++ docker/datastore/run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+if [[ -z "${DATASTORE_PROJECT_ID}" ]]; then
+    echo "Missing DATASTORE_PROJECT_ID environment variable" >&2
+    exit 1
+fi
+
+# Configure project
+gcloud config set project ${DATASTORE_PROJECT_ID}
+
+# Start emulator
+gcloud emulators datastore start \
+    --consistency=1.0 \
+    --host-port=0.0.0.0:8001 \
+    --quiet \
+    --data-dir /data
+
