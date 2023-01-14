d3cc089a052e6d1e5634154690512bd956d2a52b
echeadle <echeadle@gmail.com>
2023-01-14 11:48:54 -0700

    Finished lesson 22

--- app/bouncer/models.py
+++ app/bouncer/models.py
@@ -1,1 +1,11 @@
+"""
+Datastore Models
+"""
 
+from google.cloud import ndb
+
+class Redirect(ndb.Model):
+    """Redirect Model"""
+    name = ndb.StringProperty()
+    destination_url = ndb.StringProperty()
+    created_on = ndb.DateTimeProperty(auto_now=True)    
